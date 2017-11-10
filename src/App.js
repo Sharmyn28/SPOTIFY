0f6b323742f3e136cce2de72acd27e26e340d3a0
carina120794@gmail.com <you@example.com>
2017-11-10 17:09:53 -0500

    estilos del home

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import {connect} from 'redux-zero/react';
 import {Redirect, NavLink} from 'react-router-dom';
-import './App.css';
-
+import './css/home.css';
 const Feautures = ( {playList}) =>  {
   return (<div>
     <div className="bienvenida text-center">
@@ -15,18 +14,19 @@ const Feautures = ( {playList}) =>  {
             <li><NavLink to={"/filtro"}>Discover</NavLink></li>       
         </ul>
     </header>
-    <h1>Musica para todo los gustos...</h1>
+    <h1 align ='center'>Musica para todo los gustos...</h1>
     {
       playList.map((playList, index)=>{
         return(
           <div key={index} className="playlist">
             <div>
-              <img src={playList.image} width="150" height="150"/>
+              <NavLink to={playList.ruta}><img className="imgplay" src={playList.image} width="150" height="150"/></NavLink>
+             
             {/* <div>
               <audio src={playList.audio} controls ></audio>
             </div> */}
             <div>
-              <NavLink className="title" to={playList.ruta}>{playList.title}</NavLink >
+              <NavLink className="title" to={playList.ruta}><p className="titlemusic" align="center" >{playList.title}</p></NavLink >
             </div>
           </div>
           </div>
@@ -35,7 +35,7 @@ const Feautures = ( {playList}) =>  {
     }
      </div> 
   </div>)
-  }
+}
 
   const mapToProps = ({ playList}) => ({ playList});
   export default connect(mapToProps)(Feautures);
