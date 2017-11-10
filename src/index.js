13bf6292f40c52b597a9b1119440591b7b049ba0
aydee <aydee.qm@gmail.com>
2017-11-10 14:08:19 -0500

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Feautures from './App';
+import Filtro from './filtro';
+import Genres from './Genres';
+import News from './news';
+import {Provider} from 'redux-zero/react'
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
+import {HashRouter, Switch, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => (
+   <Provider store={store}>
+      <HashRouter>
+         <Switch>
+            <Route  exact path = "/" component={Feautures}/>
+            <Route  exact path = "/feautures" component={Feautures}/>
+            <Route  exact path = "/news" component={News}/>
+            <Route  exact path = "/filtro" component={Filtro}/>
+            <Route  exact path = "/genres" component={Genres}/>
+          </Switch>
+      </HashRouter>
+   </Provider>
+)
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
 registerServiceWorker();
+
+
