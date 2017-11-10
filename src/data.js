d3dc01ce96b4d6cd402bac5b00bb851e78dd98a0
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-11-10 15:31:11 -0500

    genres

--- src/data.js
+++ src/data.js
@@ -95,12 +95,12 @@ export const genresList = [
     },
     {
         id: 8,
-        image: "https://t.scdn.co/media/derived/latin-274x274_befbbd1…_0_0_274_274.jpg",
+        image: "https://t.scdn.co/media/derived/trending-274x274_7b238f7217985e79d3664f2734347b98_0_0_274_274.jpg",
         title:"Trending"
     },
     {
         id: 9,
-        image: "https://t.scdn.co/media/derived/latin-274x274_befbbd1…_0_0_274_274.jpg",
+        image: "https://t.scdn.co/media/derived/chill-274x274_4c46374f007813dd10b37e8d8fd35b4b_0_0_274_274.jpg",
         title:"Chill"
     },
     {
